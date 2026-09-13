7b6a2c73c507637e4221e4cc8ca59eeccfbabca6
ramonabondor <bondorramona17@gmail.com>
2026-09-13 18:39:04 +0300

    new change in store

--- src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreApplication.java
+++ src/main/java/com/ltp/globalsuperstore/GlobalSuperstoreApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GlobalSuperstoreApplication {
+	// new comment
+	// another comment
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlobalSuperstoreApplication.class, args);
